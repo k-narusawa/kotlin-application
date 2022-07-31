6e23f3ebadbd7c5b7249a4cba791d3288c592dca
narusawakohei <swkei320@gmail.com>
2022-07-31 10:47:01 +0900

    add: JWTのテストを追加

--- src/main/kotlin/com/example/kotlinapplication/util/JwtUtil.kt
+++ src/main/kotlin/com/example/kotlinapplication/util/JwtUtil.kt
@@ -4,6 +4,15 @@ import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 
 object JwtUtil {
+
+  /**
+   * JWT形式のトークンを生成
+   *
+   * @param subject サブジェクト
+   * @param expired 有効期限(分単位)
+   * @param algorithmSecret
+   * @return JWT形式のトークン
+   */
   fun createToken(subject: String, expired: Long, algorithmSecret: String): String {
     val now = DateTimeUtil.now()
     return JWT.create()
@@ -13,6 +22,13 @@ object JwtUtil {
       .sign(Algorithm.HMAC512(algorithmSecret))
   }
 
+  /**
+   * トークンを復号
+   *
+   * @param token トークン
+   * @param algorithmSecret
+   * @return トークンの復号結果
+   */
   fun decodeToken(token: String, algorithmSecret: String): String {
     return JWT.require(Algorithm.HMAC512(algorithmSecret))
       .build()
---
+++ src/test/kotlin/com/example/kotlinapplication/util/JwtUtilTest.kt
@@ -0,0 +1,47 @@
+package com.example.kotlinapplication.util
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class JwtUtilTest {
+  @Nested
+  inner class JWT形式のトークンを生成 {
+    @Test
+    fun JWT形式のトークンを生成できる() {
+      val actual = JwtUtil.createToken(
+        subject = "subject",
+        expired = 0L,
+        algorithmSecret = "secret"
+      )
+      assertThat(actual).isNotBlank
+      assertThat(actual).isNotEmpty
+      assertThat(actual).isNotNull
+    }
+  }
+
+  @Nested
+  inner class JWT形式のトークンを復号 {
+    @Test
+    fun JWT形式のトークンを復号できる() {
+      val token = JwtUtil.createToken(
+        subject = "subject",
+        expired = 1L,
+        algorithmSecret = "secret"
+      )
+      val actual = JwtUtil.decodeToken(token = token, algorithmSecret = "secret")
+      assertThat(actual).isEqualTo("subject")
+    }
+  }
+
+  @Test
+  fun 期限の切れたトークンを復号できないこと() {
+    val token = JwtUtil.createToken(
+      subject = "subject",
+      expired = 0L, // 有効期限を0分に設定
+      algorithmSecret = "secret"
+    )
+    assertThrows<Exception> { JwtUtil.decodeToken(token = token, algorithmSecret = "secret") }
+  }
+}
